f4c184642b788b1b1730d3593a28c3bdd6a9ac8e
timsainb <timsainb@gmail.com>
2019-02-20 09:16:45 -0800

    added travis CI

--- setup.py
+++ setup.py
@@ -1,10 +1,33 @@
 from setuptools import find_packages, setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='birdbrain',
     packages=find_packages(),
-    version='0.1.0',
+    version='0.1.1',
     description='Tools for using the songbird brain atlas',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/timsainb/birdbrain",
     author='Tim Sainburg',
     license='BSD-3',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        'Framework :: IPython',
+        'Intended Audience :: Scientists',
+        'Intended Audience :: Science/Research',
+        'Topic :: Multimedia :: Graphics',
+    ],
+    'install_requires': [
+        'ipywidgets>=7.0.1',
+        'ipydatawidgets',
+        'traittypes',
+        'traitlets',
+        'numpy>=1.11.0'
+    ],
+
 )
